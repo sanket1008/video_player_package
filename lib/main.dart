f81dd65919489e8ac0e0b62367fc14e20dfab146
James Dominic <58967280+JamesdominicR@users.noreply.github.com>
2023-10-13 16:47:02 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:appinio_video_player/appinio_video_player.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:video_player_package/page/video_player.dart';
+
+
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoApp(
+      debugShowCheckedModeBanner: false,
+      theme: CupertinoThemeData(
+        brightness: Brightness.dark,
+      ),
+      title: 'Appinio Video Player Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
---
+++ lib/page/video_player.dart
@@ -0,0 +1,56 @@
+import 'package:appinio_video_player/appinio_video_player.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late VideoPlayerController _videoPlayerController;
+
+  late CustomVideoPlayerController _customVideoPlayerController;
+
+  final CustomVideoPlayerSettings _customVideoPlayerSettings =
+      const CustomVideoPlayerSettings(showSeekButtons: true);
+
+  @override
+  void initState() {
+    super.initState();
+    initiateVideoPlayer();
+  }
+
+  @override
+  void dispose() {
+    _customVideoPlayerController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomVideoPlayer(
+          customVideoPlayerController: _customVideoPlayerController),
+    );
+  }
+
+  void initiateVideoPlayer() {
+    String videoUrl =
+        "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+    _videoPlayerController = VideoPlayerController.networkUrl(
+      Uri.parse(videoUrl),
+    )..initialize().then((value) => setState(() {}));
+
+    _customVideoPlayerController = CustomVideoPlayerController(
+      context: context,
+      videoPlayerController: _videoPlayerController,
+      customVideoPlayerSettings: _customVideoPlayerSettings,
+    );
+  }
+}
+
+String url =
+    "https://infyni-prod-upgrade.s3.amazonaws.com/media/courses/2023/03/Infyni_Guitar_for_Beginners_VII-16472.mp4";
